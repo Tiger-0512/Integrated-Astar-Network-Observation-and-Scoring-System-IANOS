556edd17252ca918828022a61ccee3b0cac2aa42
Taiga <matsunaga.taiga.o85@kyoto-u.jp>
2023-02-26 22:48:56 +0900

    feat: v3

--- container/nextjs/next-app/components/Header.tsx
+++ container/nextjs/next-app/components/Header.tsx
@@ -10,7 +10,7 @@ const Header: FC = () => {
         {/* <div className="bg-gradient-to-r from-[#C6279B] via-[#694CCE] to-[#02E3FF] pb-0.5 h-20">*/}
         <div className="bg-black h-full w-full flex flex-row justify-between items-center px-10">
           <div className="flex lg:w-0 lg:flex-1 items-center">
-            <img className="h-12 w-auto mr-8" src="/images/astar-logo.png" alt="" />
+            <img className="h-12 w-auto mr-8" src="/images/ianos.png" alt="" />
             <div className="font-medium text-3xl">On-chain Identity Scoring System</div>
           </div>
           <ConnectMetamaskButton />
